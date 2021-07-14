05a1e0817cda5c225412acd37b101d1892a33dab
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-07-14 16:33:33 -0700

    continud development

---
+++ story-2/src/index.js
@@ -0,0 +1,11 @@
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+        // Use $ here...
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
