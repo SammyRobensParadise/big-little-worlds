0921ac67a9b9ec562555a6c05e5d6fed833df787
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-07-14 17:43:53 -0700

    contined to develop content

--- story-2/src/index.js
+++ story-2/src/index.js
@@ -1,11 +1,36 @@
-(function() {
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-        var $ = window.jQuery;
-        // Use $ here...
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
+;(function () {
+  // Load the script
+  var script = document.createElement('SCRIPT')
+  script.src =
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'
+  script.type = 'text/javascript'
+  script.onload = function () {
+    window.$ = window.jQuery
+  }
+  document.getElementsByTagName('head')[0].appendChild(script)
+})()
+function triggerSpaceNavigation() {
+  const SPACE = 32
+  $(window).keypress(function (e) {
+    if (e.keyCode === SPACE) {
+      var t = $('#space-target').find('a')
+      if (t.length) {
+        t[0].click()
+      }
+    }
+  })
+}
+
+function handleReturnToBeginnings() {
+  const ESC = 27
+  $(window).keypress(function (e) {
+    if (e.keyCode === ESC) {
+      Engine.restart()
+    }
+  })
+}
+
+$(document).ready(function () {
+  triggerSpaceNavigation()
+  handleReturnToBeginnings()
+})
